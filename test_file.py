e7c3b6bbb4f6bc8550d66601c8569894d1b3036a
email4prasanth <email4prasanth@gmail.com>
2026-05-13 15:18:50 +0530

    checking lint

--- test_file.py
+++ test_file.py
@@ -1,13 +1,12 @@
-import math
 def display(name):
     print(f"The author is {name}")
 
 
 def add(a,b):
-    print(f"return sum is {a+b}")
+    print(f"return sum is {a +b }")
 
 name = "prasanth"
-a=2
-b=3
+a = 2
+b = 3
 display(name)
 add(a,b)
